25c12b2b40c9c2a577d107886f5b9f2cd43ee442
pavan bn <pavan6001@gmail.com>
2018-05-21 17:08:40 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 	
+echo "Hi... <br> This is a Sample PHP Application deployed by Capistrano";
+echo "<br><hr>";
+
+
+echo "The time is ...." . date("h:i:sa");
+echo "<br><hr>";
+
+
+echo "Version of App is 1.2";
+?>
+	  
